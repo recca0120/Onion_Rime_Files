ba2f457c48aeb465217a3b9b673853dae9144509
recca0120 <recca0120@gmail.com>
2023-10-17 13:43:58 +0800

    gh release

---
+++ release.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+BASEDIR=`pwd`
+FOLDER=`find ${BASEDIR} -name "電腦RIME*" -type d`
+RELEASE="${BASEDIR}/release"
+mkdir -p $RELEASE
+
+function create_zip() {
+    SOURCE=`find ${FOLDER} -name "$1" -type d`
+    cd $SOURCE
+    zip -qq -r $RELEASE/$2 ./
+}
+
+function copy() {
+    SOURCE=`find ${FOLDER} -name "$1" -type d`
+    TARGET=`find ${SOURCE} -name "$2" -type f`
+    cp -v $TARGET $RELEASE
+}
+
+create_zip "注音洋蔥純注音版_*" "bopomo_onion.zip"
+create_zip "洋蔥行列30_*" "onion-array30.zip"
+copy "Mac_官方鼠鬚管主程式" "Squirrel*"
+copy "Mac鼠鬚管外觀設定檔" "squirrel*"
+copy "Win_官方小狼毫主程式" "rime*"
+copy "Win_官方小狼毫主程式" "weasel*"
+copy "Win小狼毫外觀設定檔" "NotoSansCJK-Regular.zip"
+copy "Win小狼毫外觀設定檔" "weasel.custom.yaml"
